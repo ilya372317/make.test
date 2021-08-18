1a27ab829a442cb75098b645a39ba27c64f5e243
Ilya.Otinov <ilya.otinov@gmail.com>
2021-08-18 15:26:38 +0700

    file migrate for category, post

--- database/migrations/2021_08_18_075827_create_blog_post_models_table.php
+++ database/migrations/2021_08_18_075827_create_blog_post_models_table.php
@@ -13,9 +13,21 @@ class CreateBlogPostModelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('blog_post_models', function (Blueprint $table) {
-            $table->id();
+        Schema::create('blog_post', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('category_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content_raw');
+            $table->text('content_html');
+
+            $table->boolean('is_published')->default(false);
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
+            $table->softDeletes();
+            $table->index('is_published');
         });
     }
 
@@ -26,6 +38,6 @@ class CreateBlogPostModelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blog_post_models');
+        Schema::dropIfExists('blog_post');
     }
 }
