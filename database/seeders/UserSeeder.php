1bde592f78139f5156aff6c5cc0aeb84a1e576e2
Ilya.Otinov <ilya.otinov@gmail.com>
2021-08-18 18:28:31 +0700

    BlogCategorySeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $users = [
+            [
+                'name' => 'Неизвестный пользователь',
+                'email' => 'unknowuser@1.ru',
+                'password' => bcrypt(Str::random(16))
+            ],[
+                'name' => 'Автор',
+                'email' => 'author1@1.ru',
+                'password' => bcrypt('123123')
+            ]
+        ];
+
+        DB::table('users')->insert($users);
     }
 }
