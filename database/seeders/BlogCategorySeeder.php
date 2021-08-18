bab8a5d288d2b9fd059dd462ba8392a158f91213
Ilya.Otinov <ilya.otinov@gmail.com>
2021-08-18 15:49:25 +0700

    category seeder done

--- database/seeders/BlogCategorySeeder.php
+++ database/seeders/BlogCategorySeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use DB;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class BlogCategorySeeder extends Seeder
 {
@@ -13,6 +15,27 @@ class BlogCategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [];
+
+        $cName = 'Без категории';
+        $categories[] = [
+            'title' => $cName,
+            'slug' => Str::slug($cName),
+            'parent_id' => 0,
+        ];
+
+        for($i = 2; $i<=11; $i++){
+            $cName = "Категория №$i";
+            $parentId = $i>4 ? rand(1,4) : 1;
+
+            $categories[] = [
+                'title' => $cName,
+                'slug' => Str::Slug($cName),
+                'parent_id' => $parentId,
+            ];
+        }
+
+        DB::table('blog_category')->insert($categories);
+
     }
 }
